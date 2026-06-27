ba9aa3418bdddc78dff5b8e27e87acd34897d562
BryceMcHose <bpmchose@gmail.com>
2026-06-26 23:28:15 -0400

    debug: deployments

--- js/config.js
+++ js/config.js
@@ -15,4 +15,4 @@ const supabaseUrl = "https://kguznoohgpvswhosnokn.supabase.co";
 const supabaseKey = "sb_publishable_BJqGs3b5n3CuGDVqpzR60g_D2_ewBIH";
 const supabase = typeof supabase !== 'undefined' ? supabase.createClient(supabaseUrl, supabaseKey) : (window.supabase ? window.supabase.createClient(supabaseUrl, supabaseKey) : null);
 
-// Deployment Timestamp: 2026-06-27T03:24:20Z
+// Deployment Timestamp: 2026-06-27T03:27:50Z
